f078a8cd28a7460f3305752cbc64fb90f82e02fa
red-tomato-coder <60825238+red-tomato-coder@users.noreply.github.com>
2022-01-27 16:43:30 +0200

    some fcking lines of code

--- script.js
+++ script.js
@@ -1,5 +1,323 @@
+let bombs = 10;
 var timer = new Timer();
 timer.start();
 timer.addEventListener('secondsUpdated', function(e) {
     $('#basicUsage').html(timer.getTimeValues().toString());
 });
+
+function losed(){
+    let page = document.getElementsByTagName(document);
+}
+
+function place0(){
+    let bomber = Math.floor(Math.random() * 10)
+     if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }}
+function place1(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place2(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place3(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place4(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place5(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place6(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place7(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place8(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place9(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place10(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place11(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place12(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place13(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place14(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place15(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place16(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place17(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place18(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place19(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed")
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
+function place20(){
+    let bomber = Math.floor(Math.random() * 10)
+    
+if(bomber>5 || bombs>=1){
+        bomber = true;
+        console.log(bomber);
+        alert("You losed");
+        bomb--;
+    }
+    else{
+        bomber = false;
+        console.log(bomber);
+    }
+    
+}
